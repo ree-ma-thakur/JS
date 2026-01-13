1b8cc632bdc382c5e28691b9259ddbdc95b2a23c
Reema Thakur <reemathakur2114@gmail.com>
2026-01-13 22:19:08 +0530

    array easy

--- test.js
+++ test.js
@@ -20,3 +20,29 @@ const subArr = (arr) => {
 };
 
 console.log(subArr([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
+
+const findIntersection = (arr1, arr2) => {
+  let i = 0;
+  let j = 0;
+  const intersectionArr = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      i++;
+    } else if (arr2[j] < arr1[i]) {
+      j++;
+    } else {
+      if (
+        intersectionArr.length === 0 ||
+        intersectionArr[intersectionArr.length - 1] !== arr1[i]
+      ) {
+        intersectionArr.push(arr1[i]);
+      }
+      i++;
+      j++;
+    }
+  }
+  return intersectionArr;
+};
+const arrA = [1, 2, 2, 3, 5];
+const arrB = [2, 3, 3, 5, 7];
+console.log("Intersection:", findIntersection(arrA, arrB));
